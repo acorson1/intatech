7d4178624225ebdabfd1152f7db1449e12ba81f3
acorson1 <33963426+acorson1@users.noreply.github.com>
2021-03-17 23:45:45 +0000

    Add files via upload

---
+++ login.php
@@ -0,0 +1,11 @@
+<form action="index.php" method="post">
+        <input type="email" name="email" id="email"><br>
+        <input type="password" name="psw" id="psw"><br>
+        <input type="submit">
+    </form>
+<?php 
+    $attempt = false;
+    while ($attempt == false) {
+    
+}
+?>
